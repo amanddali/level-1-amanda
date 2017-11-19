1d5b680ccd2d14958cab27e383612315d4f01708
jtp5 <league@WTS22.attlocal.net>
2017-11-19 15:57:31 -0800

    more stuff

--- src/src/WhackAMole.java
+++ src/src/WhackAMole.java
@@ -1,20 +1,25 @@
+import java.util.Random;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class WhackAMole {
+	Random r = new Random(24);
 
 	public static void main(String[] args) {
 		WhackAMole mole = new WhackAMole();
-		mole.drawButtons();
+		mole.drawButtons(r);
 	}
 
-	public void drawButtons() {
+	public void drawButtons(Random r) {
 		JFrame frame = new JFrame("Whack a Button for Fame and Glory");
 		JPanel panel = new JPanel();
 		for (int i = 0; i < 24; i++) {
 			JButton button = new JButton();
-			panel.add(button);
+			if (i == r) {
+				panel.add(button);
+			}
 		}
 		frame.setSize(300, 350);
 		frame.setVisible(true);
