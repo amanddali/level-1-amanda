843c72f8e17b218257f4e5cf4b89e65ca9fc2dbc
runxue00 <league@WTS22.attlocal.net>
2018-05-06 15:57:21 -0700

    sutff

--- src/src/HarryPotter.java
+++ src/src/HarryPotter.java
@@ -1,29 +1,29 @@
 
 public class HarryPotter {
- 
+
 	private boolean cloakOn;
-	
+
 	HarryPotter() {
 		System.out.println("making Harry Potter...");
 	}
- 
-	void castSpell(String spell){
-System.out.println("casting spell: " + spell);
+
+	void castSpell(String spell) {
+		System.out.println("casting spell: " + spell);
 	}
- 
+
 	void makeInvisible(boolean invisible) {
 		this.cloakOn = invisible;
-		
+
 		if (cloakOn)
 			System.out.println("Harry is invisible");
 		else
 			System.out.println("Harry is visible");
 	}
- 
+
 	void spyOnSnape() {
 		System.out.println("Harry sees Professor Snape doing nefarious things.");
 	}
- 
+
 	public static void main(String[] args) {
 		HarryPotter harry = new HarryPotter();
 		harry.makeInvisible(true);
@@ -31,10 +31,5 @@ System.out.println("casting spell: " + spell);
 		harry.makeInvisible(false);
 		harry.castSpell("stupefy");
 	}
- 
+
 }
- 
- 
- 
- 
- 
